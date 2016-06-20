0f51a32b65a5d7ab2744e545c74660956f2ba309
Alex <alex@mindscape.co.nz>
2016-06-21 11:47:08 +1200

    add more sleep to allow time for server to start, processes to be killed

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ npm install koa
 npm install n
 n install latest
 n latest
-echo express >> results.txt ; node expressserver.js & sleep 1 ; ab -k -n 50000 -c 100 -t 20 http://localhost:8000/ | grep "Requests per second:" >> results.txt ; pkill -f expressserver ; sleep 1
-echo hapi >> results.txt ; node hapiserver.js & sleep 1 ; ab -k -n 50000 -c 100 -t 20 http://localhost:8000/ | grep "Requests per second:" >> results.txt ; pkill -f hapiserver ; sleep 1
-echo node >> results.txt ; node rawnodeserver.js & sleep 1 ; ab -k -n 50000 -c 100 -t 20 http://localhost:8000/ | grep "Requests per second:" >> results.txt ; pkill -f rawnodeserver ; sleep 1
-echo restify >> results.txt ; node restifyserver.js & sleep 1 ; ab -k -n 50000 -c 100 -t 20 http://localhost:8000/ | grep "Requests per second:" >> results.txt ; pkill -f restifyserver ; sleep 1
-echo koa >> results.txt ; node koaserver.js & sleep 1 ; ab -k -n 50000 -c 100 -t 20 http://localhost:8000/ | grep "Requests per second:" >> results.txt ; pkill -f koaserver ; sleep 1
+echo express >> results.txt ; node expressserver.js & sleep 5 ; ab -k -n 50000 -c 100 -t 20 http://localhost:8000/ | grep "Requests per second:" >> results.txt ; pkill -f expressserver ; sleep 5
+echo hapi >> results.txt ; node hapiserver.js & sleep 5 ; ab -k -n 50000 -c 100 -t 20 http://localhost:8000/ | grep "Requests per second:" >> results.txt ; pkill -f hapiserver ; sleep 5
+echo node >> results.txt ; node rawnodeserver.js & sleep 5 ; ab -k -n 50000 -c 100 -t 20 http://localhost:8000/ | grep "Requests per second:" >> results.txt ; pkill -f rawnodeserver ; sleep 5
+echo restify >> results.txt ; node restifyserver.js & sleep 5 ; ab -k -n 50000 -c 100 -t 20 http://localhost:8000/ | grep "Requests per second:" >> results.txt ; pkill -f restifyserver ; sleep 5
+echo koa >> results.txt ; node koaserver.js & sleep 5 ; ab -k -n 50000 -c 100 -t 20 http://localhost:8000/ | grep "Requests per second:" >> results.txt ; pkill -f koaserver ; sleep 5
